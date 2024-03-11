c0e5c6471749f27073847634a18b3da023a3b406
abhi21121211 <111838547+abhi21121211@users.noreply.github.com>
2024-03-12 00:46:17 +0530

    server connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
@@ -10,6 +11,21 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// MongoDB Atlas connection URI
+const mongoURI = "mongodb+srv://abhishekdukare689:abhi%4021121211@cluster0.thybcll.mongodb.net/Idea-clan?retryWrites=true&w=majority";
+
+// Connect to MongoDB Atlas
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB Atlas connected successfully');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB Atlas:', err);
+});
 
 // Routes
 app.use('/api/auth', authRoutes);
